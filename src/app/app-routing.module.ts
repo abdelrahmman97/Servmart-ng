da29462531c3af97b022486a5348a5a918e0e7f3
abdelrahmman97 <mabdelrahmman22@gmail.com>
2023-10-03 15:09:42 +0200

    new structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from './home-page/home-page.component';
-import { ProfileComponent } from './shared/Components/profile/profile.component';
-import { NotFoundComponent } from './shared/Components/not-found/not-found.component';
-import { LayoutComponent } from './shared/Components/layout/layout.component';
+import { HomePageComponent } from './modules/pages/home-page/home-page.component';
+import { LayoutComponent } from './core/components/layout/layout.component';
+import { ProfileComponent } from './modules/pages/profile/profile.component';
+import { NotFoundComponent } from './core/components/not-found/not-found.component';
+
 
 const routes: Routes = [
 	{ path: "", component: HomePageComponent },
 	{ path: "index", component: HomePageComponent },
 	{ path: "home", component: HomePageComponent },
+
+	// { path:"shop", component:ShopComponent},
+	// { path:"about", component:AboutComponent},
+
 	{
-		path: "u", component: LayoutComponent, children: [
-			{ path: "", component: ProfileComponent },
-			{ path: "profile", component: ProfileComponent },
+		path: "",
+		component: LayoutComponent,
+		children: [
+			{ path: "", loadChildren: () => import( './modules/modules.module' ).then( m => m.ModulesModule ) },
+			{ path: "admin", loadChildren: () => import( './modules/admin/admin.module' ).then( m => m.AdminModule ) },
 		]
 	},
+
+
+	// {
+	// 	path: "profile", component: LayoutComponent, children: [
+	// 		{ path: "", component: ProfileComponent },
+	// 	]
+	// },
 
 
 	{ path: '**', component: NotFoundComponent }
---
+++ src/app/modules/modules.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminModule } from './admin/admin.module';
+import { AuthModule } from './auth/auth.module';
+import { CustomerModule } from './customer/customer.module';
+import { VendorModule } from './vendor/vendor.module';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ServicesProviderModule } from './services-provider/services-provider.module';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { HeaderComponent } from '../core/components/header/header.component';
+import { CoreModule } from '../core/core.module';
+import { ModulesRoutingModule } from './modules-routing.module';
+
+
+
+@NgModule( {
+	declarations: [
+		HomePageComponent,
+		ProfileComponent
+	],
+	imports: [
+		CommonModule,
+		ModulesRoutingModule,
+		CoreModule,
+
+		AdminModule,
+		AuthModule,
+		CustomerModule,
+		VendorModule,
+		ServicesProviderModule,
+
+	]
+} )
+export class ModulesModule { }
