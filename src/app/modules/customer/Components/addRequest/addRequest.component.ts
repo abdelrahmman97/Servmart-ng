df2f6b8eefc6d1628ee151c9034264cfaf3ec0c0
abdelrahmman97 <mabdelrahmman22@gmail.com>
2023-10-29 15:39:01 +0200

    update addRequestComponent

--- src/app/modules/customer/Components/addRequest/addRequest.component.ts
+++ src/app/modules/customer/Components/addRequest/addRequest.component.ts
@@ -3,42 +3,58 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 
-@Component( {
+@Component({
 	selector: 'app-addRequest',
 	templateUrl: './addRequest.component.html',
 	styleUrls: ['./addRequest.component.css']
-} )
+})
 export class AddRequestComponent implements OnInit {
 
-	constructor( private router: Router, private http: HttpClient ) { }
+	constructor(private router: Router, private http: HttpClient) { }
 
 
 	step: number = 1;
 
 	AddRequestForm: FormGroup;
 	uploadedImages: Array<string> = [];
+	video: any;
 	progress: number = 0;
+	error = false;
 
 	ngOnInit() {
-		this.AddRequestForm = new FormGroup( {
-			images: new FormControl( '' ),
-			video: new FormControl( '' )
-		} );
+		this.AddRequestForm = new FormGroup({
+			images: new FormControl(''),
+			video: new FormControl('')
+		});
 	}
 
-	onFileChange( event: any ) {
+	onImagesChange(event: any) {
+		this.error = false;
 		const files = event.target.files;
 
+		if (files.length > 5) {
+			this.error = true
+			return;
+		}
+
 		// Preview the uploaded images
-		for ( const file of files ) {
+		for (const file of files) {
 			const reader = new FileReader();
 			reader.onload = () => {
-				this.uploadedImages.push( reader.result as string );
+				this.uploadedImages.push(reader.result as string);
 			};
-			reader.readAsDataURL( file );
+			reader.readAsDataURL(file);
 		}
 
 	}
 
+	onVideoChange(event: any) {
+		const file = event.target.files[0];
+	}
+
+	removeImage(image) {
+		const index = this.uploadedImages.indexOf(image);
+		this.uploadedImages.splice(index, 1);
+	}
 
 }
