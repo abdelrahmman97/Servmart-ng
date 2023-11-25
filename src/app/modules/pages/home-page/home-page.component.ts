5215efb858da62a3fbddaba4d2f64d7e93f8dc88
tyson@23 <tysonosman860@gmail.com>
2023-11-25 17:38:59 +0200

    update-himepage and Iproduct

--- src/app/modules/pages/home-page/home-page.component.ts
+++ src/app/modules/pages/home-page/home-page.component.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from 'src/app/core/models/Product/IProduct';
+import { HomePageService } from '../services/Home-page/Home-page.service';
+import { IService } from 'src/app/core/models/Service/IService';
 
 @Component({
   selector: 'app-home-page',
@@ -7,10 +10,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
-
-	constructor( private Http:HttpClient ) {}
+  Productlist:IProduct[]=[];
+  ServiceList:IService[]=[];
+  currentPage: number = 1;
+	pageSize: number = 3;
+  isThereError: boolean = false;
+	errorMessage: string = "";
+	constructor( private Homeservice:HomePageService ) {}
 	ngOnInit(): void {
-		
+		this.getproduct(this.pageSize,this.currentPage)
    	}
 
+    getproduct(Page:number,pagesize:number){
+
+      this.Homeservice.getallProduct(pagesize,Page).subscribe({
+        next:data=>{
+          this.Productlist= data as IProduct[];
+          console.log(data);
+          
+       },
+       error: error => {
+         this.isThereError = true;
+         this.errorMessage = "لقد حدث خطأ غير معروف من فضلك حاول مرة أخرى في وقت لاحق";
+         this.errorMessage = error.statusText;
+         console.log( error );
+       }
+      })
+    }
+    getallService(Ipage:number,IPageSize){
+      this.Homeservice.getallservices(Ipage,IPageSize).subscribe({
+        next:data=>{
+          this.ServiceList=data as IService[];
+          console.log(data);
+        },
+        error: error => {
+          this.isThereError = true;
+          this.errorMessage = "لقد حدث خطأ غير معروف من فضلك حاول مرة أخرى في وقت لاحق";
+          this.errorMessage = error.statusText;
+          console.log( error );
+        }
+      })
+    }
+    
 }
