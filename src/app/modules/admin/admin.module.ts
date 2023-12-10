1530f2d3fb3bdcf30c6669d1ce72447f48e0b09c
tyson@23 <tysonosman860@gmail.com>
2023-12-10 21:03:22 +0200

    update dashbord and details and home-page shop

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -6,7 +6,6 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { DashboardComponent } from './Components/dashboard/dashboard.component';
 import { OrderListComponent } from './Components/order-list/order-list.component';
 // import { SingleOrderComponent } from './Components/single-order/single-order.component';
-import { NgxChartComponent } from './Components/ngx-chart/ngx-chart.component';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { ServiceListComponent } from './Components/service-list/service-list.component';
@@ -18,15 +17,16 @@ import { SingleserviceComponent } from './Components/singleservice/singleservice
 import { SingleproductComponent } from './Components/singleproduct/singleproduct.component';
 import { SinglerequestComponent } from './Components/singlerequest/singlerequest.component';
 import { Ngxchart2Component } from './Components/ngxchart2/ngxchart2.component';
+import { NgxChartComponent } from './Components/ngx-chart-1/ngx-chart.component';
 Ngxchart2Component
 
-
+NgxChartComponent
 @NgModule({
 	declarations: [
 		AdminLayoutComponent,
 		DashboardComponent,
 		// SingleOrderComponent,
-		NgxChartComponent,
+	
 		OrderListComponent,
 		NgxChartComponent,
 		ServiceListComponent,
